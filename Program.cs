9c490faf45766f31c91d91cdf43bfef3c77fc55a
lori belle <loribelle2005@outlook.it>
2023-10-17 17:00:22 +0200

    nuoiva cosa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("hbubbiibibibibi  ubuvu ujbi");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Pipi pupu cacca!!!11!!!11!!1!!");
